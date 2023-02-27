129b170b0b1d196c31bb8bdba1dad858d534dd81
Igor Assis <123173507+NoobAuthor@users.noreply.github.com>
2023-02-27 10:19:37 -0300

    Initial project setup

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./App.css";
+import { PathfindingVisualizer } from "./components/PathFinder/PathFinder";
+
+function App() {
+  return <PathfindingVisualizer></PathfindingVisualizer>;
+}
+
+export default App;
